f7265f9911e55023dd3ad70e5e9b5d68138f5a06
Aswin <aswins945@gmail.com>
2023-12-20 17:47:09 +0530

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+     <header className='bg-red-400 w-screen'>
+      <nav>
+ h
+      </nav>
+     </header>
+    </>
+  )
+}
+
+export default Header
